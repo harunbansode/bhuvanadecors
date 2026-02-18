614134d076b5222f869d2869fbad823986afee80
harunbansode <harun.bansode@gmail.com>
2026-02-18 11:44:59 -0500

    Updated image.js file

--- sanity/lib/image.js
+++ sanity/lib/image.js
@@ -54,14 +54,23 @@
 //   return builder.image(source).auto('format');
 // }
 
-import createImageUrlBuilder from '@sanity/image-url'
-import { dataset, projectId } from '../env'
+// import createImageUrlBuilder from '@sanity/image-url'
+// import { dataset, projectId } from '../env'
 
-const builder = createImageUrlBuilder({ projectId, dataset })
+// const builder = createImageUrlBuilder({ projectId, dataset })
+
+// export const urlFor = (source) => {
+//   // Guard clause to prevent "undefined source" error
+//   if (!source) return { url: () => "" }; 
+  
+//   return builder.image(source).auto('format');
+// }
+
+import createImageUrlBuilder from "@sanity/image-url";
+import { dataset, projectId } from "../env";
+
+const builder = createImageUrlBuilder({ projectId, dataset });
 
 export const urlFor = (source) => {
-  // Guard clause to prevent "undefined source" error
-  if (!source) return { url: () => "" }; 
-  
-  return builder.image(source).auto('format');
-}
+  return builder.image(source).auto("format");
+};
