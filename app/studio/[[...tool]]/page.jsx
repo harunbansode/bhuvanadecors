57a6c185acbdb04989a1a1166535fecea54e1137
harunbansode <harun.bansode@gmail.com>
2026-02-18 12:21:06 -0500

    Fixed studio static export conflict

--- app/studio/[[...tool]]/page.jsx
+++ app/studio/[[...tool]]/page.jsx
@@ -10,15 +10,6 @@
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
 
-// Add these lines to fix the "missing generateStaticParams" error
-export const dynamicParams = false;
-
-export function generateStaticParams() {
-  return [{ tool: [] }];
-}
-
-export const dynamic = 'force-static'
-
 export { metadata, viewport } from 'next-sanity/studio'
 
 export default function StudioPage() {
