ef5f5d7f912e5d66cf864e0c5e32a56646b662ee
harunbansode <harun.bansode@gmail.com>
2026-02-18 10:38:42 -0500

    Updated portfolio page and next config

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,20 @@
 
 // export default nextConfig;
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ["cdn.sanity.io"],
+//   },
+// };
+
+// module.exports = nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // <=== Add this line
   images: {
-    domains: ["cdn.sanity.io"],
+    unoptimized: true, // <=== Highly recommended for GitHub Pages
   },
 };
 
