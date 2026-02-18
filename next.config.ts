cb24f2252ee282b1f3f31d51344715825b3960a4
harunbansode <harun.bansode@gmail.com>
2026-02-18 12:41:12 -0500

    Deleted page.jsx

--- next.config.ts
+++ next.config.ts
@@ -65,7 +65,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',      // Required for GitHub Pages
+  output: 'export', 
   images: {
     unoptimized: true,   // Required for static hosting
     remotePatterns: [
