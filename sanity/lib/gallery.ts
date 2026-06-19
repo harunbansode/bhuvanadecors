b653bfab30f68bd88d21e64ef2d640472d2b6d41
harunbansode <harun.bansode@gmail.com>
2026-06-19 00:34:02 -0400

    working on gallery

--- sanity/lib/gallery.ts
+++ sanity/lib/gallery.ts
@@ -1,29 +1,13 @@
-// import { sanityClient } from "./client";
-
-//   export async function getGallery() {
-//   const query = `*[_type == "gallery"] | order(order asc){
-//     title,
-//     description,
-//     images[]{
-//       asset, // DO NOT use asset->url here
-//       details
-//     }
-//   }`;
-//   return sanityClient.fetch(query);
-// }
-
 import { sanityClient } from "./client";
 
-export async function getGallery() {
+  export async function getGallery() {
   const query = `*[_type == "gallery"] | order(order asc){
-    _id,
     title,
     description,
     images[]{
-      ...,
+      asset, // DO NOT use asset->url here
       details
     }
   }`;
-
   return sanityClient.fetch(query);
 }
